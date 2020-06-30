15966003a952f2d79c03c3aa48c4d5148cfa01d5
BRAZILLIER Johan <j.brazillier@gmail.com>
2020-06-30 20:22:06 +0200

    Create confirmation.js

---
+++ confirmation.js
@@ -0,0 +1,14 @@
+let key = sessionStorage.getItem("infoCustomer");
+let parsedKey = JSON.parse(key);
+let prenom = parsedKey.prenom;
+let prix = parsedKey.prix;
+let orderId = parsedKey.orderId;
+
+let spanName = document.getElementById('last-name');
+spanName.innerHTML = prenom;
+
+let spanPrix = document.getElementById('prix');
+spanPrix.innerHTML = prix;
+
+let spanOrderId = document.getElementById('order-id');
+spanOrderId.innerHTML = orderId;
